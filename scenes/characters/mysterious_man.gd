fbdaf7327d96f3cc0e5c196d19aa811ff9032b78
Ali Albabily <32642133+AliAlbabily@users.noreply.github.com>
2025-11-10 23:10:14 +0100

    Level1 character is now clickable

--- scenes/characters/mysterious_man.gd
+++ scenes/characters/mysterious_man.gd
@@ -1,9 +1,21 @@
 extends Area2D
 
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 func _on_mouse_entered() -> void:
-	print("Hello")
-
+	# Lighten the image by increasing brightness (modulate makes it brighter)
+	sprite_2d.modulate = Color(1.2, 1.2, 1.2, 1.0)  # RGB > 1 makes it lighter
 
 func _on_mouse_exited() -> void:
-	print("Bye!")
+	# Restore original color
+	sprite_2d.modulate = Color(1, 1, 1, 1)  # Original color (no tint)
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		use_dialogue()
+
+func use_dialogue():
+	var dialogue = get_parent().get_node("CanvasLayer/Dialogue")
+	
+	if dialogue:
+		dialogue.start()
