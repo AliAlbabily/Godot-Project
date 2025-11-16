40964fafa1754d63023e418b43092ded7e707d38
Ali Albabily <32642133+AliAlbabily@users.noreply.github.com>
2025-11-16 01:20:55 +0100

    Moved some code to another place

--- scenes/characters/mysterious_man.gd
+++ scenes/characters/mysterious_man.gd
@@ -9,13 +9,3 @@ func _on_mouse_entered() -> void:
 func _on_mouse_exited() -> void:
 	# Restore original color
 	sprite_2d.modulate = Color(1, 1, 1, 1)  # Original color (no tint)
-
-func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton and event.pressed:
-		use_dialogue()
-
-func use_dialogue():
-	var dialogue = get_parent().get_node("CanvasLayer/Dialogue")
-	
-	if dialogue:
-		dialogue.start()
--- scenes/level_1.gd
+++ scenes/level_1.gd
@@ -1,1 +1,11 @@
 extends Node2D
+
+func _on_mysterious_man_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		use_dialogue()
+		
+func use_dialogue():
+	var dialogue = get_node("CanvasLayer/Dialogue")
+	
+	if dialogue:
+		dialogue.start()
