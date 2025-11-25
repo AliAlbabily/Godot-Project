cc62d64797536152e28442d84c501f6d5b9cf8e6
Ali Albabily <32642133+AliAlbabily@users.noreply.github.com>
2025-11-25 23:05:45 +0100

    Added a guard flag in scene transition

--- scenes/transitions/scene_transition.gd
+++ scenes/transitions/scene_transition.gd
@@ -1,6 +1,12 @@
 extends CanvasLayer
 
+var is_busy := false
+
 func change_scene(target: String) -> void:
+	if is_busy:
+		return
+	is_busy = true
+
 	$AnimationPlayer.play("dissolve")
 	await $AnimationPlayer.animation_finished  # waits for the animation to finish
 
@@ -8,3 +14,5 @@ func change_scene(target: String) -> void:
 
 	$AnimationPlayer.play_backwards("dissolve")  # play backwards
 	await $AnimationPlayer.animation_finished
+
+	is_busy = false
