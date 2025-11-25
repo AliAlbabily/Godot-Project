857d705f8a315e87301fd742ef8c14526405fea4
Ali Albabily <32642133+AliAlbabily@users.noreply.github.com>
2025-11-25 14:40:18 +0100

    Added a soundeffect to portal

--- scenes/levels/level_1.gd
+++ scenes/levels/level_1.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var dialogue_activated = false
+@onready var portal_appears: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func _on_mysterious_man_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.pressed:
@@ -18,3 +19,7 @@ func use_dialogue():
 
 func _on_dialogue_dialogue_finished() -> void:
 	$Portal.visible = true
+	play_portal_sound_effect()
+	
+func play_portal_sound_effect():
+	portal_appears.play()
