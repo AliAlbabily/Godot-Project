9adbe510c56339c56c2073da0207f57211dc2df6
Ali Albabily <32642133+AliAlbabily@users.noreply.github.com>
2026-02-09 22:42:58 +0100

    Created a class and a resource for enemy stats

---
+++ classes/enemy_stats.gd
@@ -0,0 +1,10 @@
+extends Resource
+class_name EnemyStats
+
+@export_group("Base Stats")
+@export var enemy_name: String = "Generic Enemy"
+@export var max_health: int = 100
+
+@export_group("Combat")
+@export var attack_damage: int = 10
+@export var defense: int = 5
