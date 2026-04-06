f564204fd3471f00ff68de3070e9ceb6d9ad7790
Ali Albabily <32642133+AliAlbabily@users.noreply.github.com>
2026-04-06 20:40:06 +0200

    Created a new build resource

--- classes/player_build.gd
+++ classes/player_build.gd
@@ -1,22 +1,24 @@
 extends Resource
 class_name PlayerBuild
 
-var build_name: String = "Starting Build"
+@export var build_name: String = "Starting Build"
 
 # Stat Modifiers/Base Values for this specific build
-var build_hp: int = 0
-var build_damage: int = 0
-var build_defense: int = 0
-var build_heal: int = 0
+@export_group("Base Stats")
+@export var build_hp: int = 0
+@export var build_damage: int = 0
+@export var build_defense: int = 0
+@export var build_heal: int = 0
 
-var skills: Array[String] = []
+@export_group("Skills")
+@export var skills: Array[String] = []
 
 func add_skill(skill: String) -> void:
 	if skill not in skills:
 		skills.append(skill)
 
-# This function will act as a "Skill Library"
-# It checks if the build has a specific skill
+## This function will act as a "Skill Library"
+## It checks if the build has a specific skill and call it
 func use_skill(skill_name: String):
 	if skill_name == "fireball":
 		_fireball()
